fe53ca95ff312e66e86aadd6b9751b88b7612124
Swapnil Bhosale <sswapnil.1993@gmail.com>
2018-10-15 11:57:02 +0530

    Initial commit

---
+++ src/main/java/com/swapnil/tls/fakerand/FakeRand.java
@@ -0,0 +1,58 @@
+package com.swapnil.tls.fakerand;
+
+import java.util.Arrays;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class FakeRand {
+	
+	private byte data[];
+	private int index;
+	private int numRead;
+	
+	
+	
+	
+	
+	public FakeRand() {
+	}
+
+	public FakeRand(byte[] data, int index, int numRead) {
+		super();
+		this.data = data;
+		this.index = index;
+		this.numRead = numRead;
+	}
+
+	public byte[] getData() {
+		return data;
+	}
+
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public int getNumRead() {
+		return numRead;
+	}
+
+	public void setNumRead(int numRead) {
+		this.numRead = numRead;
+	}
+
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}	
+
+}
